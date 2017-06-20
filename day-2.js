cb9d82d92e2032740fe664aaaab6313f41247388
arscholtes <alex@ascholtes.me>
2017-06-20 11:08:38 -0400

    day 2

--- day-2.js
+++ day-2.js
@@ -1,17 +1,51 @@
+// const express = require('express');
+// const app = express();
+//
+// app.get('/hello', function(req, res) {
+//   res.send('awwwwww....yeah!')
+//
+// });
+//
+//
+// app.get('salutation', function(req, res) {
+//   res.send(req.params.salutations);
+// }
+//
+//
+// app.listen(3000, function() {
+//   console.log('to make sure that, um, we aregetting something from that port.')
+// });
+
+
 const express = require('express');
 const app = express();
 
-app.get('/hello', function(req, res) {
-  res.send('awwwwww....yeah!')
 
-});
-
-
-app.get('salutation', function(req, res) {
-  res.send(req.params.salutations);
+app.get('/:operator/:num1/:num2', function (req, res) {
+  let operator = req.params.operator({
+    add: +,
+    subtract: -,
+    multiply: *,
+    divide: /
+  })
+  console.log(operator);
+  let num1 = req.params.num1;
+  console.log(num1);
+  let num2 = req.params.num2;
+  console.log(num2);
+  res.send("Answer:" + (Number(req.params.num1) + operator + Number(req.params.num1))
 }
 
 
-app.listen(3000, function() {
-  console.log('to make sure that, um, we aregetting something from that port.')
-});
+
+
+// app.get('/lorem/:count', function (req, res) {
+//   let count = req.params.count;
+//   console.log(count);
+//   let text = loremIpsum({
+//   count: count,
+//   units: 'paragraphs',
+//   format: "html"
+//   })
+//   res.send(text)
+// });
